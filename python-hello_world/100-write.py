79c907c269a7afc9cf83047f712a59b6db9efa26
eldadgishinge <e.gishinge@alustudent.com>
2023-02-05 15:33:37 -0800

    yes

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-text = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stdout.write(text)
-sys.stdout.write("\n")
+text = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(text.encode('utf-8'))
+sys.exit(1)
