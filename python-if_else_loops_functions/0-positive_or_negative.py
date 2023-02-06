6f16d4b38bd01eb374aeadb2969fa1a22aa8133d
Apple <apple@Apples-MacBook-Air.local>
2023-02-06 17:59:01 +0200

    possitive or negative number

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+number = input("Enter  a number ")
+number = int(number)
+if (number > 0):
+    print("is positive")
+elif (number == 0):
+    print("is zero")
+else :
+    print ("is negative")
