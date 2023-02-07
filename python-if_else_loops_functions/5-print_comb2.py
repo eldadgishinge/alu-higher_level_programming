e902a08e131f78e9c777f793658b23c44a39b5a3
Apple <apple@Apples-MacBook-Air.local>
2023-02-07 11:36:29 +0200

    00-99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range (0, 100):
-    if i < 10:
-        print("0{}".format(int(i)), end=", ")
-    else:
-        print("{}".format(int(i)), end=", ")
+for i in range (0,99):
+    print("{:02d}".format(int(i)), end=", ")
+print(i + 1 )
