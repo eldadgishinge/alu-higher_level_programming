f22854a2bc373554071b3c1535087ce78e1c62b5
Apple <apple@Apples-MacBook-Air.local>
2023-02-07 16:14:05 +0200

    Woow

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i+1, 10):
-        print("{:d}{:d}".format(i, j), end=", ")
-print("\b\b ")
+    for y in range(i+1,10):
+        if i == 8 and y == 9:
+            print('89')
+        else:
+            print("{:d}{:d}".format(i,y), end=", ")
