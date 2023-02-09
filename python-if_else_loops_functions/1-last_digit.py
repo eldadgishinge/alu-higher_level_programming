58fd315c9973550f06d283a2316d40f124138bc2
Apple <apple@Apples-MacBook-Air.local>
2023-02-09 14:28:38 +0200

    first

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,15 @@ import random
 number = random.randint(-10000, 10000)
 
 if number > 0:
-    digit = number % 10
+    dig = number % 10
 else:
-    digit = ((number * -1) % 10) * -1
+    dig = ((number * -1) % 10) * -1
 
-print("Last digit of {:d} is {:d}".format(number, digit), end=" ")
+print("Last digit of {:d} is {:d}".format(number, dig), end=" ")
 
-if digit == 0:
+if dig == 0:
     print("and is 0")
-elif number < 0 or digit < 6:
+elif number < 0 or dig < 6:
     print("and is less than 6 and not 0")
 else:
     print("and is greater than 5")
