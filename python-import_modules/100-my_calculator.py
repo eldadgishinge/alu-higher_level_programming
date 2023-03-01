dfa4d79c312feb98ee9ed64c16d57c3c3e1b55a9
eldadgishinge <e.gishinge@alustudent.com>
2023-03-01 08:41:18 -0800

    calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from calculator_1 import  add,sub,div,mul
-import sys
+from sys import argv
 if __name__ == "__main__":
-    num = sys.argv
+    num = argv
     num_arg = len(num)-1
     op = {"+": add, "-": sub, "*": mul, "/": div}
 
@@ -13,9 +13,9 @@ if __name__ == "__main__":
             print("Unknown operator. Available operators: +, -, * and /")
             print("1")
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
+a = int(argv[1])
+b = int(argv[3])
+print("{} {} {} = {}".format(a, argv[2], b, op[argv[2]](a, b)))
 
 
 
