81e9ffaafcd4d9c74e6d97ade6da1c280285f6eb
eldadgishinge <e.gishinge@alustudent.com>
2023-03-05 12:02:03 -0800

    \

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
+    if idx < 0:
+        return "none"
+    if idx > len(my_list):
+        return "none"
