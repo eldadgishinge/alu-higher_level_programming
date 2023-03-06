f3ae72d915f2538e5de00441a62d0a478420bd48
eldadgishinge <e.gishinge@alustudent.com>
2023-03-06 06:44:15 -0800

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     b = ""
+    if matrix is None:
+        return "$"
     for i in range(3):
         for j in range(3):
-            b += str(matrix[i][j]) + '\t'
+            b += chr(matrix[i][j] + 48) + '\t'
         print(b)
         b = ""
+
