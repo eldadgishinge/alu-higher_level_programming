ccd9e68959c8a5ae30da8a5b09b6384af35b333c
eldadgishinge <e.gishinge@alustudent.com>
2023-03-07 05:29:47 -0800

    delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -5,6 +5,6 @@ def delete_at(my_list=[], idx=0):
 
     if idx < 0 and idx >= len(my_list):
         return my_list
-    else:
-        del my_list[idx]
+    
+    del my_list[idx]
     return my_list
