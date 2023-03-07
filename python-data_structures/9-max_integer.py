303e183099d2f7d139ef5b529825b5ca6b76b447
eldadgishinge <e.gishinge@alustudent.com>
2023-03-07 08:11:58 -0800

    max

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 def max_integer(my_list=[]):
     go = 0
     for i in range(len(my_list)):
+        if range(len(my_list)) == "":
+            return None
         if my_list[i] > go:
             go = my_list[i]
-        elif range(len(my_list)) == "":
-            return None
     return go
