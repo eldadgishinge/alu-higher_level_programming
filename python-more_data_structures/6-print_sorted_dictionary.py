16e2cfa7353fa3c42dbbb0cd2b88392e2f09d995
Apple <apple@Apples-MacBook-Air.local>
2023-03-09 01:08:15 +0200

    print_sorted_dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-   for key, value in sorted(a_dictionary.items()):
-    print("{}: {}".format(key, value))
+    for key, value in sorted(a_dictionary.items()):
+     print("{}: {}".format(key, value))
