d7c8f0bc6a92a2f40a07ca3adaec27c7cbfdc96e
Apple <apple@Apples-MacBook-Air.local>
2023-03-09 14:22:54 +0200

    number_keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
      for i in a_dictionary:
-         return len(a_dictionary)
+         if len(a_dictionary) == 0:
+             return None
+
+     return len(a_dictionary)
