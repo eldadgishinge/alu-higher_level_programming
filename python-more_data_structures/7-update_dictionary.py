5d7d88b02b75fa69cca2226f55c2f5c8a755b175
eldadgishinge <e.gishinge@alustudent.com>
2023-03-09 15:11:18 +0200

    update_dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -5,4 +5,4 @@ def update_dictionary(a_dictionary, key, value):
             a_dictionary[key] = value
         if key not in a_dictionary.keys():
             a_dictionary[key] = value
-    return a_dictionaryi
+    return a_dictionary
