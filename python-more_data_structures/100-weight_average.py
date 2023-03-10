e3af1ed2dd0591cd1991570421e8027e0a790384
eldadgishinge <e.gishinge@alustudent.com>
2023-03-10 10:02:35 +0200

    weight_average

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    all = 0
+    down = 0
+    for x , y in my_list:
+        all = all + x*y
+        down = down + y
+    return all/down
