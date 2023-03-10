65cb1be4fe1ac9499951c74327c3f307dfa40798
eldadgishinge <e.gishinge@alustudent.com>
2023-03-10 16:15:06 +0200

    safe_print_integer

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if type(my_list[i]) is int:
-                print("{:d}".format(my_list[i]), end=" ")
+                print("{:d}".format(my_list[i]))
                 printed += 1
     except IndexError:
         pass
