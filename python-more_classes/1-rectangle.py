ae286738545919574a8f33fef13b13901a01db88
eldadgishinge <e.gishinge@alustudent.com>
2023-03-17 10:26:43 +0200

    1-rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,10 +8,12 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
+
     @property
     def width(self):
         """width"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """width"""
@@ -20,10 +22,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """width"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """width"""
