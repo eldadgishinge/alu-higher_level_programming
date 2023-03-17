788e171ab98ba0207ddf02cd93e8e7f1efdbd253
eldadgishinge <e.gishinge@alustudent.com>
2023-03-17 19:11:28 +0200

    5-rectangle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -50,8 +50,7 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Return the
-        """
+        """Return the """
         if self.__width == 0 or self.__height == 0:
             return ("")
         rect = []
