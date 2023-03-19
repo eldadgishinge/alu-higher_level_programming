719be63ce6639350ca7d13d94535f2ee72ffc7ed
eldadgishinge <e.gishinge@alustudent.com>
2023-03-19 21:00:54 +0200

    |

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     if not hasattr(magic_string, 'counter'): magic_string.counter = 0
-    magic_string.counter += 1; return ', '.join(['BestSchool'*magic_string.counter])
+    magic_string.counter += 1; return ', '.join(['Holberton'*magic_string.counter])
