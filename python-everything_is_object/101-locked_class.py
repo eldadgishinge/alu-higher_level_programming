018ff9d59d255f0989c1780e1d76fe0ba26e0dbb
eldadgishinge <e.gishinge@alustudent.com>
2023-03-19 20:33:10 +0200

    |

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""magic_string"""
+
 def magic_string():
+    """magic_string"""
     if not hasattr(magic_string, 'counter'):
         magic_string.counter = 0
     magic_string.counter += 1
