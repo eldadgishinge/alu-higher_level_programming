37fa0f067faceb9a20da045c4a183ff2b86088ca
eldadgishinge <e.gishinge@alustudent.com>
2023-03-20 10:52:12 +0200

    0-lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-    """
+"""
     Returns a list of available attributes and methods of an object.
     """
 
@@ -8,15 +8,15 @@ def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
     """
-    # Get the list of attributes and methods of the object
+    #Get the list of attributes and methods of the object
     attrs = dir(obj)
     filtered_attrs = []
     for attr in attrs:
         if attr.startswith(''):
             filtered_attrs.append(attr)
 
-    # Filter out any private or special attributes/methods
+    #Filter out any private or special attributes/methods
     #filtered_attrs = [attr for attr in attrs if not attr.startswith('__')]
 
-    # Return the filtered list of attributes/methods
+    #Return the filtered list of attributes/methods
     return filtered_attrs
