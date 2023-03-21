b1ea4d9cbc1d2b58cadd325ab6af23d0bd09fbc0
eldadgishinge <e.gishinge@alustudent.com>
2023-03-21 09:09:58 +0200

    Done

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 def inherits_from(obj, a_class):
     """Yes"""
-    if not type(obj) == a_class:
+    if not type(obj) == a_class and issubclass(type(obj), a_class):
         return True
     else:
         return False
