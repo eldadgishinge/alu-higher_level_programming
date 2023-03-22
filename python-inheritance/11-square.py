cd5972f796ceb48a3b9613953ba66661fe04b899
eldadgishinge <e.gishinge@alustudent.com>
2023-03-22 19:01:32 +0200

    Yes

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 10-square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Yess"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
