6602bd99f203500a4242af486e1cacd45e70c63a
eldadgishinge <e.gishinge@alustudent.com>
2023-03-22 12:36:14 +0200

    Yes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Yes"""
+
 
 BaseGeometry =__import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-
+    """Yes"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
