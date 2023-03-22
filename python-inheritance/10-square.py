b1ffe9a8a4e042fe3cff857c918d078131c341e9
eldadgishinge <e.gishinge@alustudent.com>
2023-03-22 13:44:33 +0200

    Yes

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,10 +10,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Yess"""
     def __init__(self, size):
-
+        super().integer_validator("size", size) 
         self.__size = size
         super().__init__(size, size)
-        super().integer_validator("size", size)
+
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
