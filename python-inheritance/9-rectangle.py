b1f485591456cdd86d202c46c87ced27c07ff62a
eldadgishinge <e.gishinge@alustudent.com>
2023-03-22 13:09:54 +0200

    Yes

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,8 +2,8 @@
 """YEss"""
 
 
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """YEss"""
@@ -13,7 +13,6 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
