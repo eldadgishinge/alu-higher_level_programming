747906e5cb2ff01156312754549e1c0c62e005b2
eldadgishinge <e.gishinge@alustudent.com>
2023-03-22 19:37:46 +0200

    yess

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -5,6 +5,7 @@
 class MyInt(int):
     """Yes"""
     def __eq__(self, other):
-        return self.real != other    
+        return self.real != other
+    
     def __ne__(self, other):
         return self.real == other
