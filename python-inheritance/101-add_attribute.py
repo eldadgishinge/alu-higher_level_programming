85189d24193cefffc392a0723d56e34d88358774
eldadgishinge <e.gishinge@alustudent.com>
2023-03-22 20:12:26 +0200

    yess

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -4,7 +4,11 @@
 
 def add_attribute(obj, name, value):
     """Yes"""
-    if hasattr(obj, '__dict__') or (hasattr(type(obj), '__slots__') and name in type(obj).__slots__):
+    if hasattr(obj, '__dict__'):
         obj.__setattr__(name, value)
+
+    elif (hasattr(type(obj), '__slots__') and name in type(obj).__slots__):
+        obj.__setattr__(name, value)
+
     else:
         raise TypeError("can't add new attribute")
