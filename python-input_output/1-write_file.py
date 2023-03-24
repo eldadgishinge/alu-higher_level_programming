c4a0ce4b74bd78ee02d3814ca32a29fb9ca10a36
eldadgishinge <e.gishinge@alustudent.com>
2023-03-24 12:57:16 +0200

    find_a_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """count"""
-    with open(filename, encoding="utf-8") as f:
-        count = f.write(text)
-    return count
+    """Yes"""
+    with open(filename, "w", encoding="utf-8") as f :
+        return f.write(text)
