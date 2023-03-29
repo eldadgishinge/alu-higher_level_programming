ef24cd9353b50cbd156d5e6f194d87fae7498324
eldadgishinge <e.gishinge@alustudent.com>
2023-03-29 16:08:24 -0700

    done

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,6 @@
 --  yes
-CREATE DATABASE
-    IF NOT EXISTS hbtn_0d_usa;
-    CREATE TABLE
-        IF NOT EXISTS 'hbtn_0d_usa.states'(
-            PRIMARY KEY(id),
-            id INT UNIQUE NOT NULL AUAUTO_INCREMENT,
-            name VARCHAR(256) NOT NULL
-        );
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    PRIMARY KEY(id),
+    id INT UNIQUE NOT NULL AUAUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+    );
