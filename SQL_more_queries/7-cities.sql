335f52326936e9fccdd18bbb1737dd63c7021a29
eldadgishinge <e.gishinge@alustudent.com>
2023-03-29 16:32:40 -0700

    done

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 --  yes
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE NOT NULL AUAUTO_INCREMENT,
-    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id)
-
+    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
 );
