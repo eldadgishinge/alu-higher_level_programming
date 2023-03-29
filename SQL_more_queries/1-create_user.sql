73d3d0701cd09ba6485305813aa021fefc4b2eda
eldadgishinge <e.gishinge@alustudent.com>
2023-03-29 02:27:23 -0700

    user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,6 +4,6 @@ IDENTIFIED BY user_0d_1_pwd;
 
 GRANT ALL PRIVILEGES
 ON *.*
-TO user_0d_1@localhost;
-IDENTIFIED BY user_0d_1_pwd;
+TO user_0d_1@localhost
+IDENTIFIED BY user_0d_1_pwd
 FLUSH PRIVILEGES;
