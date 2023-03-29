91c2cbd9a27c11615df743347c4775847e40028c
eldadgishinge <e.gishinge@alustudent.com>
2023-03-29 02:28:53 -0700

    user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
--- YES
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY user_0d_1_pwd;
+-- yes
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES
 ON *.*
-TO user_0d_1@localhost
-IDENTIFIED BY user_0d_1_pwd
+TO 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
