38d97acc9e427bfb9976b7649f896820c97d9760
eldadgishinge <e.gishinge@alustudent.com>
2023-03-31 08:10:14 -0700

    yes

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,10 @@
--- yes
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC, tv_genres.id ASC;
+-- List all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT `name`, SUM(`rate`) AS `rating`
+    FROM `tv_genres` AS g
+	INNER JOIN `tv_show_genres` AS s
+	ON s.`genre_id` = g.`id`
+
+	INNER JOIN `tv_show_ratings` AS r
+	ON r.`show_id` = s.`show_id`
+GROUP BY `name`
+ORDER BY `rating` DESC;i
