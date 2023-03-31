713df9c15d101a48ad44061d83ef1641d6b86f94
eldadgishinge <e.gishinge@alustudent.com>
2023-03-31 07:55:59 -0700

    yes

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
---yes
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
-JOIN tv_show_ratings
-     ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_shows.title
-ORDER BY rating DESC;
+-- yes
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
