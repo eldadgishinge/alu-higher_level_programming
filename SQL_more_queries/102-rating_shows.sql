1bfeef89013dc583b31d6d7baa96b38d7ed306ed
eldadgishinge <e.gishinge@alustudent.com>
2023-03-31 08:03:04 -0700

    yes

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
--- yes
-SELECT `title`, SUM(`rate`) AS `rating`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_ratings` AS r
-       ON t.`id` = r.`show_id`
- GROUP BY `title`
- ORDER BY `rating` DESC;
+-- A script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- Query script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title;
