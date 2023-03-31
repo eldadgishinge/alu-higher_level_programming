7e7ffa40e6c8cb430fd9542bc5936b4c9d2aa5fc
eldadgishinge <e.gishinge@alustudent.com>
2023-03-31 00:35:56 -0700

    done

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- yes
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON cities.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
