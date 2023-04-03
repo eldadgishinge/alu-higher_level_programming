dc6fe2e8a60b9bbf3197e6de2cf886e0f37ce006
eldadgishinge <e.gishinge@alustudent.com>
2023-04-03 11:13:16 +0200

    |

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get the byte size of the HTTP response header for a given URL.
-url="$1"; response=$(curl -s -w "%{size_download}" -o /dev/null "$url"); echo "Response size: $response bytes"
+curl -s  "$1" | wc -c
