2a6645ec294ee05db7b2e9e41c4d4c511ea4677b
eldadgishinge <e.gishinge@alustudent.com>
2023-04-04 11:03:30 +0200

    done

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #curl
-curl -sF "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
