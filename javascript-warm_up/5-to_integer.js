c7759081c2ffcee76821e3a37189d20aa60333e2
eldadgishinge <e.gishinge@alustudent.com>
2023-06-12 14:39:23 +0200

    js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const prt = process.argv[2]
+const prt = parseInt(process.argv[2])
 
-const z = Number(prt)
 
-console.log(typeof(z))
 
-if (Number){
+if (prt){
     console.log('My number: ' + prt);
 } else {
     console.log('Not a number');
