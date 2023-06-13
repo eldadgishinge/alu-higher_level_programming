4855e3331e9d9e1fe1112f94896c8aa1803d347c
eldadgishinge <e.gishinge@alustudent.com>
2023-06-13 16:01:50 +0200

    done

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle{
-    constructor (w, l){
+    constructor (w, h){
         this.width = w;
-        this.length = l;
+        this.height = h;
     }
 };
-
-
-
-
-
-
-
-
