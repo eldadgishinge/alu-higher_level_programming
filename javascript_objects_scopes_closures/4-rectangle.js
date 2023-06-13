8d62094bb34dcfc268692e6e2c1604d78011d852
eldadgishinge <e.gishinge@alustudent.com>
2023-06-13 18:00:29 +0200

    js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -13,10 +13,8 @@ module.exports = class Rectangle {
     this.width = this.height;
     this.height = c;
   }
-
   double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
   }
-
 };
