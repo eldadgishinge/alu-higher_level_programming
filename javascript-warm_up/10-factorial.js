eea728ece924a2ebd95abbce90ba6932fc4eac26
eldadgishinge <e.gishinge@alustudent.com>
2023-06-13 12:53:20 +0200

    js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function factorial (number) {
-    if (isNaN(number) || number <= 1) {
-      return 1;
-    }
-    return number * factorial(number - 1);
+  if (isNaN(number) || number <= 1) {
+    return 1;
   }
+  return number * factorial(number - 1);
+}
 
- console.log(factorial(process.argv[2]));
+console.log(factorial(process.argv[2]));
---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function add (a,b){
+    let sum  = a + b;
+    console.log(sum);
+}
+
+add(3,5);
