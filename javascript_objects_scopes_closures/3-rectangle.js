932c9508ae1bc2fc7b34dba5dedaf59fd18f6e1a
eldadgishinge <e.gishinge@alustudent.com>
2023-06-13 17:28:41 +0200

    js

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    }
+
+    print(){
+        for (let i = 0; i < w; i++){
+            for (let i = 0; i< h; i++){
+                console.log('X'.repeat(w,h))
+            }
+
+        }
+
+    }
+  };
+
