0618d6f3acca20d40beb44b8af39f71d7be322a2
eldadgishinge <e.gishinge@alustudent.com>
2023-06-13 18:18:31 +0200

    js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,30 +1,9 @@
 #!/usr/bin/node
 
-const Rectangle = require("./4-rectangle");
+const Rectangle = require("./4-rectangle")
 
-module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-    }
-
-    print () {
-      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-    }
-
-    rotate () {
-      const c = this.width;
-      this.width = this.height;
-      this.height = c;
-    }
-
-    double () {
-      this.width = this.width * 2;
-      this.height = this.height * 2;
+module.exports = class Square extends Rectangle {
+    constructor(size){
+        super(size,size);
     }
   };
-
-  class Square extends Rectangle{
-    constructor(size){
-        super(size,size)
-    }
-  }
