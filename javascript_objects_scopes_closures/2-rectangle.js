8cb8769d4dee3649868603f378e44386ce081bf7
eldadgishinge <e.gishinge@alustudent.com>
2023-06-13 16:36:57 +0200

    done

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@ module.exports = class Rectangle {
     constructor (w, h) {
       this.width = w;
       this.height = h;
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
         let obj = {}
     }
     }
