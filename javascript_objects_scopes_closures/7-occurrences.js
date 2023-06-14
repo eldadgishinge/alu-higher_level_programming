fe690ac72889a1f69be788121a96606f8524926b
eldadgishinge <e.gishinge@alustudent.com>
2023-06-14 13:08:56 +0200

    js

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    var y = 0
+    let y = 0
     for (let i = 0; i < list.length; i++) {
 
         if (list[i] == searchElement) {
