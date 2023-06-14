1721d42d2c348c2321a82b87a87808a37a128906
eldadgishinge <e.gishinge@alustudent.com>
2023-06-14 13:58:38 +0200

    js

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list){
-let j = list.length - 1 ;
-let lest2 = []
-for(let i = j; i >= 0; i-- ) {
-  lest2.push(list[i])
+  const j = list.length - 1;
+  const lest2 = [];
+  for (let i = j; i >= 0; i--) {
+    lest2.push(list[i]);
   }
-console.log(lest2)
-}
-
+return lest2;
+};
