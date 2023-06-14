7497dd68d7c21c1d6967f292a677c55eb4365c41
eldadgishinge <e.gishinge@alustudent.com>
2023-06-14 12:48:13 +0200

    js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
-  charPrint(c) {
-    if (c === undefined ) {
-          for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-
-    }else{
-          for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
-
-        }
-}
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
 }
