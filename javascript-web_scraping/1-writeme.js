6689a67c9559754cca76be5280c4758b8da8a41a
eldadgishinge <e.gishinge@alustudent.com>
2023-06-15 12:10:04 +0200

    js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const ks = require('ks')
-ks.writefile( process.argv[3], 'utf8', error =>{
+const fs = require('fs')
+fs.writeFile( process.argv[3], 'utf8', error =>{
     if (error) console.log(error);
 });
