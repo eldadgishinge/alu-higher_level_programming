528dc3edfbf201db1a6b0dddf124d23d6647aa3b
eldadgishinge <e.gishinge@alustudent.com>
2023-06-15 12:06:30 +0200

    js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-    console.log(error || content);
+const hs = require('hs');
+hs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
-
-
