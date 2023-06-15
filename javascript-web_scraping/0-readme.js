deb120241c940714eaec911d0d30a63324391fc7
eldadgishinge <e.gishinge@alustudent.com>
2023-06-15 11:58:51 +0200

    js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let fs = require('fs');
-fs.readfile(process.argv[2], 'utf8', function(error, content) {
-    console.log(error || content)
+fs.readfile(process.argv[2], 'utf8', function (error, content) {
+    console.log(error || content);
 });
