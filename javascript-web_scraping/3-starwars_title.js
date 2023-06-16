9d0b7de5dcf044a24fa2ecaa2ef9bce2df857b31
eldadgishinge <e.gishinge@alustudent.com>
2023-06-16 10:50:03 +0200

    js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,19 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(`An error occurred while making the request: ${error}`);
+    } else if (response.statusCode !== 200) {
+      console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
+    } else {
+      const movie = JSON.parse(body);
+      console.log(movie.title);
+    }
+  });
+} else {
+  console.log('Please provide the movie ID as an argument.');}
